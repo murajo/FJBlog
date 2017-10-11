a370366411cabba9fa7be21a17ba70b9da939f1e
murajo <31980685+murajo@users.noreply.github.com>
2017-10-11 12:05:16 +0900

    豊春作成
    移行作業

--- fuel/app/classes/controller/bord.php
+++ fuel/app/classes/controller/bord.php
@@ -1,17 +1,31 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: x16n064
- * Date: 2017/09/27
- * Time: 11:47
- */
+
 class Controller_Bord extends Controller
 {
 
     public function action_index()
     {
         $view = View::forge('bord/index.php');
+        //if(Input::post('action')=='save'){
+        if((!empty(Input::post('user'))) == '1' && (!empty(Input::post('text'))) == '1'){
+        $user = Input::post('user');
+        $text = Input::post('text');
+        date_default_timezone_set('Asia/Tokyo');
+        $time=date("Y/m/d H:i");
+        $dbobj2 = new Model_bord();
 
+        $dbobj2->username = $user;
+        $dbobj2->text = $text;
+        $dbobj2->created_at = $time;
+        
+        
+        // $dbobj2->set('username',$user);
+        // $dbobj2->set('text',$text);
+        // $dbobj2->set('created_at','now');
+        $dbobj2->save();
+        }
+        $query = Model_bord::query()->get();
+        $view->set('dbObj',$query);
         return $view;
     }
 }
--- fuel/app/views/bord/index.php
+++ fuel/app/views/bord/index.php
@@ -1,7 +1,49 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: x16n064
- * Date: 2017/09/27
- * Time: 11:48
- */
+<!DOCTYPE HTML>
+<html lang="ja">
+    <meta charset="UTF-8">
+<?=Asset::css('style.css')?>
+<head>
+    <link rel="SHORTCUT ICON" HREF="assets/img/favicon.ico">
+    <title>FJBlog</title>
+</head>
+<body>
+<!-- バージョン表記 -->
+<div class="ver" align="right">
+    ばーじょんは1.0だよ！
+</div>
+
+<div class="gazou" align="center">
+<?=Asset::img('FJBlog.png')?>
+</div>
+<!-- トップ等のアクセス -->
+<table border="2" align="center" class="aaa">
+　<tr class="main" data-ng-href="">
+　　<th class="home"><a href="">ホーム</a></th>
+　　<th class="twitter"><a href="">twitter</a></th>
+    <th class"keijibann"><a href="">けいじばん</a></th>
+　</tr>
+</table><!-- トップ等のアクセス　ここまで -->
+
+<h2>～みんなの投稿～</h2>
+
+<!-- ユーザーの投稿 -->
+ <?php foreach ($dbObj as $result): ?>
+<table style="border-collapse:collapse;border:none;" align="center" class="bbb">
+　<tr class="space">
+　　<th class="user"><?= $result->username ?></th>
+　　<th class="text"><?= $result->text ?></th>
+    <th class="timestamp"><?= $result->created_at ?></th>
+　</tr>
+<?php endforeach ?>
+
+</table><!-- 投稿テーブルここまで -->
+
+<div class="toukou">
+<form action="bord.php" method="post" onsubmit="return false"><!-- Enterが押されてもsubmitしない -->
+    <label><input type="text" name="user" placeholder="ユーザー名"></label>
+    <p><textarea name="text" placeholder="本文を入力してください"></textarea></p>
+    <input class="sousin" type="button" name="save" value="送信" onclick="submit()"><!-- ボタンが押されたときsubmitする -->
+</div>
+
+</body>
+</html>
